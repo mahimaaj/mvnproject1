cac427bd87af4716f532140cadbd69cbe9973e44
mahimaa <mahimaaj@gmail.com>
2017-08-13 22:02:56 -0400

    third commit

--- src/test/java/com/alg/mvnproject/test1.java
+++ src/test/java/com/alg/mvnproject/test1.java
@@ -2,6 +2,7 @@ package com.alg.mvnproject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class test1 {
